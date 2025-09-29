49671f1a8957f857ef608632e89d49c5e3b82fbe
BOUAISSA <didik.bouaissa@gmail.com>
2025-09-29 13:08:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello! ${name}!`);
+  const name = process.env.NAME || 'WorldV2';
+  res.send(`Hello ${name}!`);
 });
 
 app.listen(port, () => {
